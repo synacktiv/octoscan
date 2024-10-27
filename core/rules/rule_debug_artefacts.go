4ac9edf79b63c7c41813c6a26f127f5249e423bd
hugo-syn <hugo.vincent@synacktiv.com>
2024-10-27 21:52:42 +0100

    fix: add filter-trigger to the debug-artefacts rule

--- core/rules/rule_debug_artefacts.go
+++ core/rules/rule_debug_artefacts.go
@@ -8,20 +8,35 @@ import (
 
 type RuleDebugArtefacts struct {
 	actionlint.RuleBase
+	filterTriggers []string
+	skip           bool
 }
 
 // NewRuleOIDCAction creates new RuleOIDCAction instance.
-func NewRuleRuleDebugArtefacts() *RuleDebugArtefacts {
+func NewRuleRuleDebugArtefacts(filterTriggers []string) *RuleDebugArtefacts {
 	return &RuleDebugArtefacts{
 		RuleBase: actionlint.NewRuleBase(
 			"debug-artefacts",
 			"Check for workflow that upload artefacts.",
 		),
+		filterTriggers: filterTriggers,
+		skip:           false,
 	}
+}
+
+func (rule *RuleDebugArtefacts) VisitWorkflowPre(n *actionlint.Workflow) error {
+	// check on event and set skip if needed
+	rule.skip = skipAnalysis(n, rule.filterTriggers)
+
+	return nil
 }
 
 // VisitStep is callback when visiting Step node.
 func (rule *RuleDebugArtefacts) VisitStep(n *actionlint.Step) error {
+	if rule.skip {
+		return nil
+	}
+
 	e, ok := n.Exec.(*actionlint.ExecAction)
 	if !ok || e.Uses == nil {
 		return nil
